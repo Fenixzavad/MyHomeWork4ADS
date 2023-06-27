7c4e7757b88d5aad83434575afb03a8e9ba13c54
fenixzava <fenixzava@gmail.com>
2023-06-27 22:21:57 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BTree<Integer> tree = new BTree<>();
+
+        tree.add(11);
+        tree.add(5);
+        tree.add(3);
+        tree.add(4);
+        tree.add(2);
+        tree.add(8);
+        tree.add(9);
+        tree.add(7);
+        tree.add(1);
+        tree.add(6
+        );
+
+        tree.print();
+
+    }
+}
